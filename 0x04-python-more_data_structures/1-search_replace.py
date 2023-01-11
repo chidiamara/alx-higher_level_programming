7bb7732e407af11372b1906b1b307666ac0f141c
Chidiamara <chidiamaraekejiuba@gmail.com>
2023-01-11 14:21:26 +0000

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in my_list:
-        if i is search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-            return new_list
+    new_list = [x if x != search else replace for x in my_list]
+    return new_list
