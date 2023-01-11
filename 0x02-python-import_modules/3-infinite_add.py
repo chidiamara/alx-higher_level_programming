7da2f0f1f90b178d22d6d7fcd5b73d066f66b21a
Chidiamara <chidiamaraekejiuba@gmail.com>
2023-01-11 11:12:23 +0000

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        sum = 0
-         from sys import argv
+    from sys import argv
+    sum = 0
+    if len(argv) > 1:
          for i in range(1, len(argv)):
              sum += int(argv[i])
          print(sum)
