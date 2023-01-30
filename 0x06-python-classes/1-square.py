57589b0ef00a5ca8d93690b1f34aaac09cfb2f5b
Chidiamara <chidiamaraekejiuba@gmail.com>
2023-01-30 13:22:18 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """The Square class defines a square by size (private) """
+    """ Square class defines a square by size (private) """
     def __init__(self, size):
         """ initializes the object """
         self.__size = size
